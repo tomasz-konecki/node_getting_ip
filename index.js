6e0d5e4573de62f18759236f9b0942f9df276ffb
Konecki <koneckit@emea.roche.com>
2018-06-08 11:57:01 +0200

    Added code

--- index.js
+++ index.js
@@ -1,27 +1,74 @@
-const express = require("express");
-const fetch = require("fetch");
+const https = require("https");
+const http = require("http");
+const chalk = require("chalk");
+const fs = require("fs");
+const path = require("path");
 
-const app = express();
+getAPI = () => {
+  const url =
+    "https://pixabay.com/api/?key=9220972-14c941a191779d5f50bce3cd4&q=yellow+flowers&image_type=photo";
 
-app.get("/", (req, res) => {
-  //reset console
+  const myWriteStream = fs.createWriteStream(
+    path.join(__dirname, "/writeMe.txt")
+  );
+
+  https
+    .get(url, res => {
+      // It's important to set encoding to utf8 (otherwisre we get data in Buffer format)
+      res.setEncoding("utf8");
+
+      console.log("statusCode:", res.statusCode);
+      console.log("headers:", res.headers);
+
+      res.on("data", d => {
+        let data = "";
+        //   console.log(chalk.red("chunk of data received"));
+        data = data + d;
+
+        // Lines 23 and 24 do exactly the same thing
+        //   myWriteStream.write(d);
+        res.pipe(myWriteStream);
+        console.log(data);
+
+        return data;
+      });
+
+      res.on("end", () => {
+        console.log(chalk.rgb(0, 0, 255)("End..."));
+      });
+    })
+    .on("error", e => {
+      console.error(e);
+    });
+};
+
+reset = () => {
   console.log("\033c");
+};
 
-  let ip = req.header("x-forwarded-for") || req.connection.remoteAddress;
-  console.log(ip);
+const server = http.createServer((req, res) => {
+  console.log("request was made", req.url);
 
-  var ipaddress = (
-    req.headers["x-forwarded-for"] ||
-    req.connection.remoteAddress ||
-    req.socket.remoteAddress ||
-    req.connection.socket.remoteAddress
-  ).split(",")[0];
+  switch (req.url) {
+    case "/":
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end("<h1>Main page</h1>");
+    case "/api":
+      getAPI();
+    case "/reset":
+      reset();
+    case "/page":
+      res.writeHead(200, { "Content-Type": "text/html" });
+      let file = fs.readFileSync("./index.html");
+      console.log(file);
 
-  console.log(ipaddress);
-
-  res.end();
+      res.end(file);
+    default:
+      res.writeHead(404, { "Content-Type": "text/html" });
+      res.end("<h3>Not found...</h3>");
+  }
 });
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+server.listen(7000, () => {
+  console.log("server running...");
 });
